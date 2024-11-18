d158335b0997855cfac941a5e41c53c31e35fb43
Daniel Park <Dpark39@myseneca.ca>
2024-11-17 23:15:15 -0500

    Add ejs to dependencies

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ const PORT = process.env.PORT || 8080;
 //}));
 //app.set('view engine', 'hbs');
 //app.set('views', path.join(__dirname, 'views'));
-
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
